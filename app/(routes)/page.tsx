b86d122b5ee74991b8c977b720065505b7ba5b89
mohammadtonor <mtonor1368@gmail.com>
2023-12-10 23:25:42 +0330

    fix

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -4,8 +4,6 @@ import Billboard from "@/components/billboard";
 import ProductList from "@/components/product-list";
 import Container from "@/components/ui/container";
 
-export const reValidate = 0;
-
 const HomePage = async () => {
   const products = await getProducts({ isFeatured: true, isArchived: false });
   const billboard = await getBillboard("e0d42fea-92a3-41d8-bd97-e6cb4f6d72a4");
